f50b3e611a842f7a88cd6470e7e0e6a819ec79ba
Mohsin Ahmed Shaikh <mohsinshaikh786@gmail.com>
2023-09-23 16:08:08 +0300

    Update main.tf
    fixed issue with security group definitions

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name          = "blog"
   description   = "Allow http and http in. Allow everyting out"
-  vpc_id        = "data.aws_vpc.default.id
+  vpc_id        = "data.aws_vpc.default.id"
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
